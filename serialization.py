657a53f84fecaa13fe59ca51fe9486a9fb697d61
tianyuningmou <tianyuningmou2009@126.com>
2018-03-09 12:46:36 +0800

    add gitignore

---
+++ serialization.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved
+
+FILE: serialization.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2018/3/9 下午12:09
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2018/3/9 下午12:09
+"""
+
+
