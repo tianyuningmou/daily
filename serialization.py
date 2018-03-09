5e602a332f551f925252c8e23e7160082c84548a
tianyuningmou <tianyuningmou2009@126.com>
2018-03-09 14:19:34 +0800

    serialization

--- serialization.py
+++ serialization.py
@@ -17,4 +17,36 @@ CHANGE:  :
 MODIFIED: : @Time : 2018/3/9 下午12:09
 """
 
+"""
+    Pickle序列化：
+        该模块实现了基本的数据序列化和反序列化，通过pickle模块的序列化操作我们能够将程序中运行的对象信息保存到文件中去，永久存储；
+        通过pickle模块的反序列化操作，我们可以取得保存的数据。
+    JSON序列化：
+        json是一种轻量级的数据交换格式，易于阅读和编写。
+    marshal序列化：
+        marshal并不通用，因为使用marshal序列化的二进制数据格式还没有文档化。
+"""
 
+import marshal
+import json
+import pickle
+
+origin_list = [1, (2, 'string'), {'key': 'value'}]
+
+json_encode = json.dumps(origin_list)
+json_decode = json.loads(json_encode)
+print(json_decode)
+
+with open('ser_file.marshal', 'wb') as file:
+    marshal.dump(origin_list, file)
+
+with open('ser_file.marshal', 'rb') as file:
+    marshal_list = marshal.load(file)
+    print(marshal_list)
+
+with open('ser_file.txt', 'wb') as file:
+    pickle.dump(origin_list, file)
+
+with open('ser_file.txt', 'rb') as file:
+    pickle_list = pickle.load(file)
+    print(pickle_list)
