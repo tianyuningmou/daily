b57976ed6ec63f2a545640705ffa5ee5d1898ad0
tianyuningmou <tianyuningmou2009@126.com>
2018-04-12 12:02:06 +0800

    __init__

---
+++ Python/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved
+
+FILE: __init__.py.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2018/4/12 下午12:01
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2018/4/12 下午12:01
+"""
+
+
