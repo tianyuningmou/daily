251be1d591a736a868794d8fe622084aecfc586f
tianyuningmou <tianyuningmou2009@126.com>
2018-05-10 14:21:36 +0800

    redis_operation

---
+++ DB_operation/redis_operation.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved
+
+FILE: redis_operation.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2018/5/10 下午2:21
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2018/5/10 下午2:21
+"""
+
+
