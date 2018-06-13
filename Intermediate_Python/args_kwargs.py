d143128c8b0172a96c792626e5d2fa2094f6e32a
tianyuningmou <tianyuningmou2009@126.com>
2018-06-13 14:44:29 +0800

    changed gitignore

---
+++ Intermediate_Python/args_kwargs.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved
+
+FILE: args_kwargs.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2018/6/13 下午2:25
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2018/6/13 下午2:25
+"""
+
+"""
+    *args是用来发送一个非键值对的可变数量的参数列表给一个函数
+    **kwargs允许你将不定长度的键值对, 作为参数传递给一个函数   
+"""
+
+
+def use_var_args(f_arg, *args):
+    print('first normal args:', f_arg)
+    for arg in args:
+        print('another arg through *args:', arg)
+
+
+use_var_args('python', 'eggs', 'birds', 'dogs')
+
+
+def greet(**kwargs):
+    for key, value in kwargs.items():
+        print('{0}=={1}'.format(key, value))
+
+
+greet(name='python')
