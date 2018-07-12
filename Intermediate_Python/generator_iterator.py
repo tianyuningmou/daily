081faefb5b1904c45056891643ad3fb9554ab4a3
tianyuningmou <tianyuningmou2016@126.com>
2018-07-12 15:41:42 +0800

    generator_iterator

---
+++ Intermediate_Python/generator_iterator.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright () 2018
+
+All rights reserved
+
+FILE: generator_iterator.py
+AUTHOR: tianyuningmou
+DATE CREATED:  @Time : 2018/6/14 下午3:32
+
+DESCRIPTION:  .
+
+VERSION: : #1 
+CHANGED By: : tianyuningmou
+CHANGE:  : 
+MODIFIED: : @Time : 2018/6/14 下午3:32
+"""
+
+
