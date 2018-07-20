152ca9cd5f9dd785c8ffabdd78c2f3466fe92983
tianyuningmou <tianyuningmou2016@126.com>
2018-07-20 12:11:08 +0800

    client_eg

--- Async_IO_Http/client_eg.py
+++ Async_IO_Http/client_eg.py
@@ -27,6 +27,7 @@ async def fetch(session, url):
         async with session.get(url) as response:
             return await response.text()
 
+
 async def main():
     async with aiohttp.ClientSession() as session:
         html = await fetch(session, 'http://www.baidu.com')
